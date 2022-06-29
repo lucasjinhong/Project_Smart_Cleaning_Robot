8e3e39ce3aabb7be961e50f0c47ea81cb611631c
lucasjinhong <55587534+lucasjinhong@users.noreply.github.com>
2022-06-29 17:21:12 +0800

    use yolo at main script

--- Python/main.py
+++ Python/main.py
@@ -4,7 +4,7 @@ import json
 import requests
 import torch
 from collections import Counter
-
+from PIL import Image, ImageDraw
 
 def main():
     # 選擇第二隻攝影機
@@ -12,9 +12,12 @@ def main():
 
     url = "http://35.77.46.57:3000/home/data/627bb1b46013fa5837d41835"
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5n')
-    #yolo = cv2.dnn.readNet('./yolov5n.pt', './yolov5n.yaml')
 
+    model = torch.hub.load('../yolo/yolov5', 'custom', path='../yolo/yolov5/runs/train/yolo_road_det5/weights/best.pt', source='local')
+    model.conf = 0.9
+    model.source = 0
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
     wait = 0
 
     while True:
@@ -23,71 +26,45 @@ def main():
         orig_frame = frame.copy()
 
         result = model(orig_frame)
-        data = Counter(result.pandas().xyxy[0].name).most_common()
+        jsonData = json.loads(result.pandas().xyxy[0].to_json(orient="records"))
 
-        '''
-        classes = []
+        hCenter, wCenter = 320, 240
 
-        with open("./coco.yaml", 'r') as f:
-            classes = f.read().splitlines()
+        if len(jsonData):
+            for j in jsonData:
+                xmax, ymax, xmin, ymin = int(j['xmax']), int(j['ymax']), int(j['xmin']), int(j['ymin'])
+                name = j['name']
+                conf = str('{0:.2}'.format(j['confidence']))
 
-        blob = cv2.dnn.blobFromImage(orig_frame, 1/255, (320,320), (0,0,0), swapRB=True, crop=False)
-        yolo.setInput(blob)
-        output_layers_name = yolo.getUnconnectedOutLayersNames()
-        layeroutput = yolo.forward(output_layers_name)
+                height = xmax - xmin
+                width = ymax - ymin
 
-        boxes = []
-        confidences = []
-        class_ids = []
-        width = 320
-        height = 320
+                xmaxD = abs(xmax - wCenter)
+                xminD = abs(xmin - wCenter)
+                ymaxD = abs(ymax - wCenter)
+                yminD = abs(ymin - wCenter)
 
-        for output in layeroutput:
-            for detection in output:
-                score = detection[5:]
-                class_id = np.argmax(score)
-                confidence = score[class_id]
+                #if abs(xmaxD - xminD) < 10 and  abs(ymaxD - yminD) < 10:
+                    #print(xmaxD, xminD, ymaxD, yminD)
 
-                if confidence > 0.7:
-                    center_x = int(detection[0] * width)
-                    center_y = int(detection[0] * height)
-                    w = int(detection[0] * width)
-                    h = int(detection[0] * height)
+                #pixel = height * width
 
-                    x = int(center_x - w / 2)
-                    y = int(center_y - h / 2)
+                #if(pixel > 100*100):
+                    #print('yes')
+                
+                cv2.rectangle(orig_frame, (xmin,ymin), (xmax,ymax), (0, 255, 0), 2)
+                cv2.putText(orig_frame, name + conf, (xmin,ymin), font, 1, (0, 255, 0), 2, cv2.LINE_AA)  
 
-                    boxes.append([x,y,w,h])
-                    confidences.append(float(confidence))
-                    class_ids.append(class_id)
-
-        indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-        font = cv2.FONT_HERSHEY_PLAIN
-        colors = np.random.uniform(0, 255, size=(len(boxes), 3))
-
-        for i in indexes.flatten():
-            x,y,w,h = boxes[i]
-
-            label = str(classes[class_ids[i]])
-            confi = str(round(confidences[i], 2))
-            color = colors[i]
-
-            cv2.rectangle(orig_frame, (x,y), (x+w, y+h), color, 1)
-            cv2.putText(orig_frame, label +" "+confi, (x, y+20), font, 2, (255,255,255), 1)
-        '''
-
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        if data:
-            cv2.putText(orig_frame, data[0][0], (10, 30), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
-
+        cv2.rectangle(orig_frame, (hCenter - 5, wCenter - 5), (hCenter + 5, wCenter + 5), (0, 0, 255), 1)
         cv2.imshow('Detected Objects', orig_frame)
 
+        '''
         if wait < 100:
             wait += 1
 
         else:
             if data:
-                payload = {'obejct': [data[0][0]]}
+                payload = {'object': [data[0][0]]}
                 print(json.dumps(payload))
 
             # Send http request
@@ -95,6 +72,7 @@ def main():
                 print(f"Status Code: {r.status_code}, Response: {r.json()}")
 
             wait = 0
+        '''
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
